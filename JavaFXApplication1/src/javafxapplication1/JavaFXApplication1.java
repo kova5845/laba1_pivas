b0248dd7ad75cfdcc77bffa80973cc438c2d2909
alexey <lesha.kovalenko.1999@mail.ru>
2019-02-16 09:35:26 +0300

    second task is ready

--- JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
+++ JavaFXApplication1/src/javafxapplication1/JavaFXApplication1.java
@@ -9,8 +9,9 @@ import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.control.TextField;
 import javafx.scene.control.Button;
-import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 /**
@@ -23,13 +24,17 @@ public class JavaFXApplication1 extends Application {
     public void start(Stage primaryStage) {
         Button button1 = new Button("first text");
         Button button2 = new Button("second text");
+        TextField text = new TextField();
+        button1.setFocusTraversable(false);
+        
         button1.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
-                String s = button1.getText();
-                button1.setText(button2.getText());
-                button2.setText(s);
+                
+               button2.setText(text.getText());
+               text.clear();
+               
             }
         });
         
@@ -43,9 +48,27 @@ public class JavaFXApplication1 extends Application {
             }
         });
         
-        FlowPane root = new FlowPane(button1, button2);
+        button1.setMinWidth(100);
+        button1.setMinHeight(50);
+        button2.setMinWidth(100);
+        button2.setMinHeight(50);
+        button1.setLayoutX(50);
+        button1.setLayoutY(50);
+        button2.setLayoutX(150);
+        button2.setLayoutY(50);
         
-        Scene scene = new Scene(root, 300, 250);
+        
+        text.setMinSize(200, 50);
+        text.setLayoutX(50);
+        text.setLayoutY(100);
+        
+        Pane root = new Pane();
+        
+        root.getChildren().add(button1);
+        root.getChildren().add(button2);
+        root.getChildren().add(text);
+        
+        Scene scene = new Scene(root, 300, 200);
         
         primaryStage.setTitle("Hello World!");
         primaryStage.setScene(scene);
